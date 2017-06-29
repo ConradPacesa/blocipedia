c4f7277ce73b3c4824fcef850d51952555918f57
Conrad <conradpacesa@gmail.com>
2017-06-28 21:52:33 -0400

    Fixed sign up issue with user name

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :charges, only: [:new, :create, :destroy]
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   root 'welcome#index'
 end
